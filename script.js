d840c442f06cf212041be63d8ca7e1ab161efb1d
colemic2006 <251512021+colemic2006@users.noreply.github.com>
2026-03-09 22:28:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-// Animation removed for stability
+const cards = document.querySelectorAll(".timeline-card");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      entry.target.classList.add("visible");
+    }
+  });
+},{ threshold:0.2 });
+
+cards.forEach(card => observer.observe(card));
